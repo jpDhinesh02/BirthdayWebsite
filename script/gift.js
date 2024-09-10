9e75e66f1ce4bcf40b54e929f9b9079d04decc06
jpDhinesh02 <testing.iddhi@gmail.com>
2024-09-10 12:31:33 +0530

    Update gift.js

--- script/gift.js
+++ script/gift.js
@@ -93,7 +93,7 @@ async function sendEmail() {
         body: JSON.stringify({
           to: "j9894330583@gmail.com,karthikv.healthguru@gmail.com",
           subject: "Happiee Birthday to you...",
-          text: `Hi saadi,\n\nYour order for gift placed\nYou will get your gift shortly\nIf you want to thanks call me +918825886153`,
+          text: `Hi saadi,\n\nYour order for gift placed\nGet ready—your gift will be arriving soon!\nIf you want to thanks call me +918825886153`,
       }
     );
 

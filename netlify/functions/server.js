00e94b33b774bd1579c0396c9aa9ec0b4b4f1fc0
jpDhinesh02 <testing.iddhi@gmail.com>
2024-09-10 11:57:36 +0530

    Add files via upload

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -7,13 +7,13 @@ exports.handler = async (event) => {
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
-        user: "jpdhinesh2002@gmail.com",
-        pass: "hffi ddmx udjj hfeu",
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
       },
     });
 
     const mailOptions = {
-      from: "jpdhinesh2002@gmail.com",
+      from: process.env.EMAIL_USER,
       to,
       subject,
       text,
